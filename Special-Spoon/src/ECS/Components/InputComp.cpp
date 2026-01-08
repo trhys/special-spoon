29fc38241bb99d2d2608067d0fdd0a62a6bf6a2d
Tim Reese <treese1993@gmail.com>
2026-01-08 12:37:59 +0000

    fix: crash in inputcomp edit Fixes #46

--- Special-Spoon/src/ECS/Components/InputComp.cpp
+++ Special-Spoon/src/ECS/Components/InputComp.cpp
@@ -40,52 +40,49 @@ namespace Spoon
 
             if(ImGui::BeginPopup(editWindow))
             {   
-                if (ImGui::BeginChild("Available Keys"))
+                ImGui::BeginChild("Available Keys");
+                if (ImGui::BeginListBox("##keylist"))
                 {
-                    if (ImGui::BeginListBox("##keylist"))
+                    for (int k = static_cast<int>(sf::Keyboard::Key::A); k <= static_cast<int>(sf::Keyboard::KeyCount); k++)
                     {
-                        for (int k = static_cast<int>(sf::Keyboard::Key::A); k <= static_cast<int>(sf::Keyboard::KeyCount); k++)
-                        {
-                            sf::Keyboard::Key key = static_cast<sf::Keyboard::Key>(k);
-                            if (m_KeyBindings.find(key) != m_KeyBindings.end())
-                                continue;
+                        sf::Keyboard::Key key = static_cast<sf::Keyboard::Key>(k);
+                        if (m_KeyBindings.find(key) != m_KeyBindings.end())
+                            continue;
 
-                            std::string keyName = sf::Keyboard::getDescription(static_cast<sf::Keyboard::Scancode>(key)).toAnsiString();
-                            ImGui::PushID(keyName.c_str());
-                            const bool is_selected = (editKey == key);
-                            if (ImGui::Selectable(keyName.c_str(), is_selected))
-                            {
-                                editKey = key;
-                            }
-                            if (is_selected)
-                                ImGui::SetItemDefaultFocus();
-                            ImGui::PopID();
-                        }
-                        ImGui::EndListBox();
-                    }
-                    ImGui::EndChild();
-                }
-                if (ImGui::BeginChild("Available Actions"))
-                {
-                    if (ImGui::BeginListBox("##actionlist"))
-                    {
-                        for (const auto& [name, id] : ActionRegistry::Get().m_NameToID)
+                        std::string keyName = sf::Keyboard::getDescription(static_cast<sf::Keyboard::Scancode>(key)).toAnsiString();
+                        ImGui::PushID(keyName.c_str());
+                        const bool is_selected = (editKey == key);
+                        if (ImGui::Selectable(keyName.c_str(), is_selected))
                         {
-                            const char* actionName = name.c_str();
-                            ImGui::PushID(actionName);
-                            const bool is_selected = (editAction == id);
-                            if (ImGui::Selectable(actionName, is_selected))
-                            {
-                                editAction = id;
-                            }
-                            if (is_selected)
-                                ImGui::SetItemDefaultFocus();
-                            ImGui::PopID();
+                            editKey = key;
                         }
-                        ImGui::EndListBox();
+                        if (is_selected)
+                            ImGui::SetItemDefaultFocus();
+                        ImGui::PopID();
                     }
-                    ImGui::EndChild();
+                    ImGui::EndListBox();
                 }
+                ImGui::EndChild();
+                
+                ImGui::BeginChild("Available Actions");
+                if (ImGui::BeginListBox("##actionlist"))
+                {
+                    for (const auto& [name, id] : ActionRegistry::Get().m_NameToID)
+                    {
+                        const char* actionName = name.c_str();
+                        ImGui::PushID(actionName);
+                        const bool is_selected = (editAction == id);
+                        if (ImGui::Selectable(actionName, is_selected))
+                        {
+                            editAction = id;
+                        }
+                        if (is_selected)
+                            ImGui::SetItemDefaultFocus();
+                        ImGui::PopID();
+                    }
+                    ImGui::EndListBox();
+                }
+                ImGui::EndChild();
                 if(ImGui::Button("Submit"))
                 {
                     m_KeyBindings.erase(currentKey);
@@ -114,52 +111,49 @@ namespace Spoon
             ImGui::OpenPopup(newBindWindow);
         if (ImGui::BeginPopup(newBindWindow))
         {
-            if (ImGui::BeginChild("Available Keys"))
+            ImGui::BeginChild("Available Keys");
+            if (ImGui::BeginListBox("##keylist"))
             {
-                if (ImGui::BeginListBox("##keylist"))
+                for (int k = static_cast<int>(sf::Keyboard::Key::A); k <= static_cast<int>(sf::Keyboard::KeyCount); k++)
                 {
-                    for (int k = static_cast<int>(sf::Keyboard::Key::A); k <= static_cast<int>(sf::Keyboard::KeyCount); k++)
-                    {
-                        sf::Keyboard::Key key = static_cast<sf::Keyboard::Key>(k);
-                        if (m_KeyBindings.find(key) != m_KeyBindings.end())
-                            continue;
+                    sf::Keyboard::Key key = static_cast<sf::Keyboard::Key>(k);
+                    if (m_KeyBindings.find(key) != m_KeyBindings.end())
+                        continue;
 
-                        std::string keyName = sf::Keyboard::getDescription(static_cast<sf::Keyboard::Scancode>(key)).toAnsiString();
-                        ImGui::PushID(keyName.c_str());
-                        const bool is_selected = (editKey == key);
-                        if (ImGui::Selectable(keyName.c_str(), is_selected))
-                        {
-                            editKey = key;
-                        }
-                        if (is_selected)
-                            ImGui::SetItemDefaultFocus();
-                        ImGui::PopID();
-                    }
-                    ImGui::EndListBox();
-                }
-                ImGui::EndChild();
-            }
-            if (ImGui::BeginChild("Available Actions"))
-            {
-                if (ImGui::BeginListBox("##actionlist"))
-                {
-                    for (const auto& [name, id] : ActionRegistry::Get().m_NameToID)
+                    std::string keyName = sf::Keyboard::getDescription(static_cast<sf::Keyboard::Scancode>(key)).toAnsiString();
+                    ImGui::PushID(keyName.c_str());
+                    const bool is_selected = (editKey == key);
+                    if (ImGui::Selectable(keyName.c_str(), is_selected))
                     {
-                        const char* actionName = name.c_str();
-                        ImGui::PushID(actionName);
-                        const bool is_selected = (editAction == id);
-                        if (ImGui::Selectable(actionName, is_selected))
-                        {
-                            editAction = id;
-                        }
-                        if (is_selected)
-                            ImGui::SetItemDefaultFocus();
-                        ImGui::PopID();
+                        editKey = key;
                     }
-                    ImGui::EndListBox();
+                    if (is_selected)
+                        ImGui::SetItemDefaultFocus();
+                    ImGui::PopID();
                 }
-                ImGui::EndChild();
+                ImGui::EndListBox();
             }
+            ImGui::EndChild();
+            
+            ImGui::BeginChild("Available Actions");
+            if (ImGui::BeginListBox("##actionlist"))
+            {
+                for (const auto& [name, id] : ActionRegistry::Get().m_NameToID)
+                {
+                    const char* actionName = name.c_str();
+                    ImGui::PushID(actionName);
+                    const bool is_selected = (editAction == id);
+                    if (ImGui::Selectable(actionName, is_selected))
+                    {
+                        editAction = id;
+                    }
+                    if (is_selected)
+                        ImGui::SetItemDefaultFocus();
+                    ImGui::PopID();
+                }
+                ImGui::EndListBox();
+            }
+            ImGui::EndChild();
             if(ImGui::Button("Submit"))
             {
                 m_KeyBindings[editKey] = ActionType{ editAction };
