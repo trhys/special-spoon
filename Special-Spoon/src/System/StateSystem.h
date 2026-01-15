dc23120c4884bbfa7c7fb88043236127f59784c3
trhys <treese1993@gmail.com>
2026-01-15 00:29:07 +0000

    wip: reworking action sys

--- Special-Spoon/src/System/StateSystem.h
+++ Special-Spoon/src/System/StateSystem.h
@@ -4,63 +4,20 @@
 
 namespace Spoon
 {
-    class SceneManager;
-    class SystemManager;
-
     class StateSystem : public ISystem
     {
     public:
         StateSystem() : ISystem::ISystem("State") {}
         ~StateSystem() {}
 
-        void Update(sf::Time tick, EntityManager& manager) override
-        {
-            auto& stateActionArray = manager.GetArray<StateActionComp>(StateActionComp::Name);
-            for(size_t in = 0; in < stateActionArray.m_Components.size(); in++)
-            {
-                StateActionComp& stateComp = stateActionArray.m_Components[in];
-                UUID ID = stateActionArray.m_IndexToId[in];
+        void Update(sf::Time tick, EntityManager& manager) override;
 
-                auto& actionsBuffer = manager.GetActionsBuffer();
-                auto found = actionsBuffer.find(ID);
-                if(found != actionsBuffer.end())
-                {
-                    std::string triggeredAction = found->second;
-                    if(stateComp.m_Actions.count(triggeredAction))
-                    {
-                        std::string targetState = stateComp.m_Actions[triggeredAction];
-                        if(targetState == "Quit")
-                        {
-                            quitFlag = true;
-                            return;
-                        }
-                        RequestSceneChange(targetState);
-                        break;
-                    }
-                }
-            }
-
-            if(sceneChangeFlag)
-            {
-                return;
-            }
-        }
-
-        void RequestSceneChange(const std::string& sceneID)
-        {
-            m_CurrentScene = sceneID;
-            sceneChangeFlag = true;
-        }
-
-        bool IsQuitRequested() { return quitFlag; }
-        void ConsumeQuitFlag() { quitFlag = false; }
-        bool IsSceneChangeRequested() { return sceneChangeFlag; }
-        std::string ConsumeChangeRequest() 
-        { 
-            sceneChangeFlag = false;
-            return m_CurrentScene; 
-        }
-
+        void RequestSceneChange(const std::string& sceneID);
+        bool IsSceneChangeRequested();
+        std::string ConsumeChangeRequest();
+        bool IsQuitRequested();
+        void ConsumeQuitFlag();
+        
     private:
         std::string m_CurrentScene;
         bool sceneChangeFlag = false;
