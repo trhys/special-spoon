876388b7b612957b471d52e3aec42829ef6437a9
Tim Reese <treese1993@gmail.com>
2026-09-17 07:51:44 -0500

    Fix include statement formatting in Renderable.h

--- Special-Spoon/src/Core/Renderer/Renderable.h
+++ Special-Spoon/src/Core/Renderer/Renderable.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "Core/EntityManager/EntityManager.h
+#include "Core/EntityManager/EntityManager.h"
 #include "SFML/Graphics/RenderTarget.hpp"
 #include "SFML/Graphics/RenderStates.hpp"
 
