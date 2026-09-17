026f5c721a19b3bc654cee943b8d3f041ea42b5c
Tim Reese <treese1993@gmail.com>
2026-09-17 12:12:25 -0500

    Update Policies.h

--- Special-Spoon/src/Core/Project/Policies.h
+++ Special-Spoon/src/Core/Project/Policies.h
@@ -1,7 +1,14 @@
 #pragma once
 
 namespace Spoon {
-	enum class RenderType {
-		Isometric = 1
-	}
+// interface for depth sorting policies
+	class SortPolicy {
+		public:
+			virtual void ComputeDepth(std::vector<Renderable>& renderables) = 0;
+	};
+
+	class IsometricProjection : public SortPolicy {
+		public:
+			void ComputeDepth(std::vector<Renderable>& renderables) override;
+	};
 }
