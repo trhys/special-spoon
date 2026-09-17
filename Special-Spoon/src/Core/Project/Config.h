1112bc0f3cb707002b72c01fe7c32ce3c93903c6
Tim Reese <treese1993@gmail.com>
2026-09-17 13:16:39 -0500

    wip: added serialization macro to project config

--- Special-Spoon/src/Core/Project/Config.h
+++ Special-Spoon/src/Core/Project/Config.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "Core/Project/Policies.h"
+#include <nlohmann/json.hpp>
 
 namespace Spoon {
 	struct ProjectConfig {
 		bool PhysicsEnabled = false;
 		ActiveSortPolicy SortPolicy = 0;
 	}
+
+	NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(ProjectConfig, PhysicsEnabled, ActiveSortPolicy)
 }
